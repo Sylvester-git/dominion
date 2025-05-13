84e620afccd8384d045348b92f4d183b330dea23
sylvesterpaul David <87859727+Sylvester-git@users.noreply.github.com>
2025-05-13 22:37:31 +0100

    fix: ensure timer is canceled on HomePage dispose to prevent memory leaks

--- lib/features/home.dart
+++ lib/features/home.dart
@@ -28,6 +28,12 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(

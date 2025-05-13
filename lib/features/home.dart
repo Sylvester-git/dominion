e8ae8b59374074576b0c5d2be3a31a702a2f7453
sylvesterpaul David <87859727+Sylvester-git@users.noreply.github.com>
2025-05-13 23:11:01 +0100

    feat: enhance error handling in HealthMetrics display with user-friendly message and refresh prompt

--- lib/features/home.dart
+++ lib/features/home.dart
@@ -35,40 +35,56 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          CustomHomeAppBar(),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Health Metrics',
-                  style: TextStyle(fontWeight: FontWeight.w500),
-                ),
-              ],
+    return RefreshIndicator(
+      onRefresh: () async {
+        context.read<GetSensorDataCubit>().getSensorData();
+      },
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CustomHomeAppBar(),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Health Metrics',
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
             ),
-          ),
-          BlocBuilder<GetSensorDataCubit, GetSensorDataState>(
-            builder: (context, getsensorstate) {
-              if (getsensorstate is GetSensorDataLoading ||
-                  getsensorstate is GetSensorDataInitial) {
-                return Center(child: CircularProgressIndicator());
-              } else if (getsensorstate is GetSensorDataLoaded) {
-                return HealthGridView();
-              } else if (getsensorstate is GetSensorDataError) {
-                return Center(
-                  child: Text('Error: ${getsensorstate.failure.message}'),
-                );
-              }
-              return Center(child: Text('Error: }'));
-            },
-          ),
-        ],
+            BlocBuilder<GetSensorDataCubit, GetSensorDataState>(
+              builder: (context, getsensorstate) {
+                if (getsensorstate is GetSensorDataLoading ||
+                    getsensorstate is GetSensorDataInitial) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (getsensorstate is GetSensorDataLoaded) {
+                  return HealthGridView();
+                } else if (getsensorstate is GetSensorDataError) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.error, color: Colors.red, size: 50),
+                        SizedBox(height: 10),
+                        Text(
+                          'Error: ${getsensorstate.failure.message}',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        Text('Swipe down to refresh'),
+                      ],
+                    ),
+                  );
+                }
+                return Center(child: Text('Error: }'));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
